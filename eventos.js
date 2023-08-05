6b5f61297fc53e46552b2ac5855dcdb93c82241e
Dacobits <103399198+Dacobits@users.noreply.github.com>
2023-08-05 20:50:21 -0300

    solucion click

--- eventos.js
+++ eventos.js
@@ -1,5 +1,11 @@
 let elDiv = document.getElementById('contenedor')
+let boton = document.getElementById('boton');
 
 elDiv.addEventListener('click', function(){
-    alert('¡Hola, Soy el Div!')
+    alert('Hola, Soy el Div!')
 })
+
+boton.addEventListener('click', function(event) {
+    event.stopPropagation();
+    alert('¡Hola!');
+});
